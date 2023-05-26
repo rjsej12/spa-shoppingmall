cd182180e56112729608b09ff7bccf17713fbca1
rjsej12 <rjsej12@naver.com>
2023-05-27 00:27:26 +0900

    [REFACTOR] : Font-weight 추가 및 레이아웃 디자인 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
           rel="stylesheet"
         />
       </Head>
