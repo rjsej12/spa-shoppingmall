1a792ed099516650f739fb527d9ef433dfa03c89
rjsej12 <rjsej12@naver.com>
2023-05-26 23:00:00 +0900

    [FEAT] : 버튼 마크업, 스타일링 변경 및 클릭 시 이동 기능 추가

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,16 +1,23 @@
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { FiShoppingCart } from 'react-icons/fi';
+import { useRouter } from 'next/router';
 import styles from './index.module.scss';
 
 export default function LayoutHeader() {
+  const router = useRouter();
+
   return (
     <nav className={styles.wrapper}>
       <div className={styles.icon_wrapper}>
-        <RxHamburgerMenu />
+        <button type="button" onClick={() => router.push('/shop')}>
+          <RxHamburgerMenu />
+        </button>
       </div>
       <h1 className={styles.title}>Refilled</h1>
       <div className={styles.icon_wrapper}>
-        <FiShoppingCart />
+        <button type="button" onClick={() => router.push('/cart')}>
+          <FiShoppingCart />
+        </button>
       </div>
     </nav>
   );
